3e53eb5730702f0039d39f63c03a3cedba3ab4db
unknown <thangbui.071192@gmail.com>
2021-05-01 22:33:08 +0700

    first commit

---
+++ database/migrations/2021_04_19_095625_create_infos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('infos', function (Blueprint $table) {
+            $table->id();
+            $table->int('user_id')->nullable();
+            $table->int('customer_id')->nullable();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('infos');
+    }
+}
